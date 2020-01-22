5d5724b06cca32987ae63226d981410e60b80be0
Lukas Burkhard <lukas.burkhard@seantis.ch>
2020-01-22 09:37:52 +0100

    Corrects typo

--- generate.py
+++ generate.py
@@ -26,7 +26,7 @@ if final.exists():
 
 final.mkdir()
 
-check_call("make clean-generated", stdout=DEVNULL, stderr=DEVNULL, shell=True)
+check_call("make clean", stdout=DEVNULL, stderr=DEVNULL, shell=True)
 
 print("Generating maps for {}".format(year))
 check_call(
